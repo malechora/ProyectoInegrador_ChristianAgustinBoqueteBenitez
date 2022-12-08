9bd07460fc1d5aeb31f394aa6fb08a5193497784
malechora <agustinxxzasd@gmail.com>
2022-12-08 20:08:39 -0300

    agregamos angular

---
+++ ProyectoIntegrador_ChristianAgustin/src/app/components/hardysoftskills/hardysoftskills.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hardysoftskills',
+  templateUrl: './hardysoftskills.component.html',
+  styleUrls: ['./hardysoftskills.component.css']
+})
+export class HardysoftskillsComponent {
+
+}
